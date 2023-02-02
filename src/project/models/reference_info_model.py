a0331ec1996f5e73aaa18750ee94a2baaa82927d
ann-cooper <cooperannc@gmail.com>
2023-02-02 17:17:00 -0500

    First pass document model for reference info

--- src/project/models/reference_info_model.py
+++ src/project/models/reference_info_model.py
@@ -1,13 +1,39 @@
-from src.project.services import mgdb
+import datetime
+from typing import Optional
 
+from attrs import define, field
 
-class ReferenceInfo(mgdb.Document):
+def convert_info_dict(cls):
+    """Handle nested object,
+    
+    Reference
+    ---------
+    https://github.com/python-attrs/attrs/issues/140
+    """
+    def converter(obj):
+        if isinstance(obj, cls):
+            return obj
+        else:
+            print(obj)
+            return cls(**obj)
+    return converter
+
+@define
+class InfoDict:
+    source: str = field()
+    link: str = field()
+    info: str = field()
+
+@define
+class ReferenceInfo:
     """Clue reference research info."""
+    page: int = field()
+    clue: str = field()
+    created_date: datetime.date = field(default=None)
+    reference: Optional[InfoDict] = field(default=None, converter=convert_info_dict(cls=InfoDict))
+    _id: Optional[str] = field(default=None)
 
-    _id = mgdb.ObjectIdField(required=True)
-    page = mgdb.IntField(required=True)
-    clue = mgdb.StringField(required=True)
-    reference = mgdb.DictField(required=True, default={})
-    created_date = mgdb.ComplexDateTimeField(required=True)
+    def __attrs_post_init__(self):
+        if not self.created_date:
+            self.created_date = datetime.date.today().isoformat()
 
-    meta = {"collection": "references"}
